d753ee8216d7c51d6a2a8425f76842d61936cdf2
charupraneeth <charupraneeth@gmail.com>
2023-02-25 22:33:09 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,83 @@
+// websockets.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	// A mutex to synchronize access to connections.
+	// This is needed because the main goroutine and
+	// the goroutines that handle connections run in
+	// parallel.
+	connectionsLock sync.Mutex
+
+	// A slice to store all connections.
+	connections []*websocket.Conn
+)
+
+func main() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+		// Add the new connection to the list.
+		connectionsLock.Lock()
+		connections = append(connections, conn)
+		connectionsLock.Unlock()
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				// Remove the connection from the list if there was an error.
+				connectionsLock.Lock()
+				for i, c := range connections {
+					if c == conn {
+						connections = append(connections[:i], connections[i+1:]...)
+						break
+					}
+				}
+				connectionsLock.Unlock()
+
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			connectionsLock.Lock()
+			for _, c := range connections {
+				// Skip the sender.
+				if c == conn {
+					continue
+				}
+
+				if err = c.WriteMessage(msgType, msg); err != nil {
+					// Remove the connection from the list if there was an error.
+					for i, cc := range connections {
+						if cc == c {
+							connections = append(connections[:i], connections[i+1:]...)
+							break
+						}
+					}
+				}
+			}
+			connectionsLock.Unlock()
+		}
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/", http.StripPrefix("/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
